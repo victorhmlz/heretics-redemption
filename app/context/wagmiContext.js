432f847a8c338e061f1f906723394deb846830c3
victorhmlz <victorhmlz@gmail.com>
2024-10-04 01:46:50 +0200

    Updating wallet connectors

--- app/context/wagmiContext.js
+++ app/context/wagmiContext.js
@@ -4,9 +4,8 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { createAppKit } from '@reown/appkit/react'; 
 import { polygon, sepolia } from '@reown/appkit/networks';
 import React from 'react'; 
-import { http, WagmiProvider, CreateConnectorFn } from 'wagmi';
+import { WagmiProvider } from 'wagmi';
 import { WagmiAdapter } from '@reown/appkit-adapter-wagmi';
-import { injected, metaMask, safe, walletConnect, coinbaseWallet } from 'wagmi/connectors'
 
 const queryClient = new QueryClient();
 
@@ -24,30 +23,17 @@ const metadata = {
 // 3. Set the networks
 const networks = [ sepolia ]
 
-const connectors = [];
-connectors.push(injected({ shimDisconnect: true })); // Inyectado (por ejemplo, para Metamask directamente)
-connectors.push(walletConnect({ projectId, metadata, showQrModal: false })); // Conector de WalletConnect
-connectors.push(coinbaseWallet({ appName: metadata.name, appLogoUrl: metadata.icons[0] })); // Conector de Coinbase Wallet
-connectors.push(metaMask({ shimDisconnect: true })); // Conector específico de MetaMask
-connectors.push(safe()); // Conector Safe (Gnosis Safe)
-
 // 4. Create Wagmi Adapter
 const wagmiAdapter = new WagmiAdapter({
-  transports: {
-    [sepolia.id]: http(),
-  },
   networks,
-  connectors,
   projectId,
   ssr: true
 });
 
-export const config = wagmiAdapter.wagmiConfig;
-
 // 5. Create modal
 createAppKit({
   adapters: [wagmiAdapter],
-  networks: [sepolia],
+  networks,
   projectId,
   metadata,
   features: {
